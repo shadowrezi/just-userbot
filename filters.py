2afa006348b1525b48503ae9b46ea7eb3d516472
shadowrezi <urijozimko4@gmail.com>
2023-11-01 19:24:54 +0200

    Add checking on None message

--- filters.py
+++ filters.py
@@ -4,5 +4,7 @@ from pyrogram.types import Message
 
 def in_message(text):
     async def filter(flt, _, message: Message):
+        if message.text is None:
+            return False
         return flt.text in message.text
     return filters.create(filter, text=text)  # text=text, we get it by `flt`
