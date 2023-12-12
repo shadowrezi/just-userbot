e17cfc3f511c97537359ea4b218d3ea780621187
ShadowRezi <urijozimko4@gmail.com>
2023-12-12 16:41:33 +0200

    Removing calculator.py plugin

--- plugins/calculator.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-
-@Client.on_message(filters.command('', ['~']) & filters.me)
-async def calculator(_, message: Message):
-    print(message.command[1:])
-    await message.reply(eval(message.text[2:]), quote=True)
-
