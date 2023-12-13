d35605007d86b040b4769ec1de5da0ce6f6e64d0
shadowrezi <129873777+shadowrezi@users.noreply.github.com>
2023-12-13 18:14:03 +0200

    Update TextToSpeech.py

--- plugins/TextToSpeech.py
+++ plugins/TextToSpeech.py
@@ -1,33 +1,36 @@
-import os
+from os import remove, listdir
 
-from pyrogram import Client, filters
+from pyrogram import Client
+from pyrogram.filters import command, me, private
 from pyrogram.types import Message
 
 from gtts import gTTS
 
 
 @Client.on_message(
-    filters.command(
+    command(
         commands=['gtts'],
         prefixes=['.', '/']
-    )
+    ) & (me | private)
 )
-async def text_to_speech(_, message: Message):
+async def text_to_speech(_: Client, message: Message):
     data = ' '.join(message.command[1:])
     filename = 'downloads/temp.mp3'
-
-    # await message.edit(f'**{data}**')
     
     try:
         gTTS(
             text=data,
-            lang='ru'
+            lang='uk'
         ).save(filename)
     
         await message.reply_voice(
             voice=filename,
             quote=True,
-            caption='Voiced by gTTS\n@ShadowRazea'
+            caption='Voiced by gTTS'
         )
     finally:
-        os.remove(filename)
+        dirs = listdir('.')
+        if 'downloads' not in dirs:
+            return
+        if filename in listdir('downloads'):
+            os.remove(filename)
