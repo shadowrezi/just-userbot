7a86c0049143cc5b1cbaf52905556e99430ec1f2
ShadowRezi <urijozimko4@gmail.com>
2023-12-22 15:18:47 +0200

    Add GUI manager for bot

---
+++ manager.py
@@ -0,0 +1,63 @@
+from importlib import reload
+from multiprocessing import Process
+
+import customtkinter as ctk
+
+import userbot
+
+
+class BotControlApp:
+
+    def __init__(self, master):
+        ctk.set_default_color_theme('green')
+
+        self.master = master
+        self.master.title("Managing bot")
+        # self.master.config(bg='#072a47')
+
+        self.label = ctk.CTkLabel(master, text='Stopped')
+        self.label.pack(pady=10)
+
+        ctk.CTkButton(master, text='Start', command=self.start_bot).pack(pady=10)
+        ctk.CTkButton(master, text='Stop', command=self.stop_bot).pack(pady=10)
+        ctk.CTkButton(master, text='Restart', command=self.restart_bot).pack(pady=10)
+
+        self.is_bot_runned = False
+        self.bot = None
+
+    def start_bot(self):
+        if not self.is_bot_runned:
+            self.is_bot_runned = True
+            self.update_label()
+            
+            reload(userbot)
+            self.bot = Process(
+                target=userbot.main
+            )
+            self.bot.start()
+
+    def stop_bot(self):
+        if self.is_bot_runned:
+            self.is_bot_runned = False
+            self.update_label()
+
+            self.bot.terminate()
+            self.bot.join()
+
+            print('Stopping bot!')
+
+    def restart_bot(self):
+        if self.is_bot_runned:
+            self.stop_bot()
+        self.start_bot()
+
+    def update_label(self):
+        self.label.configure(
+            text='Started' if self.is_bot_runned else 'Stopped'
+            )
+
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    app = BotControlApp(root)
+    root.mainloop()
