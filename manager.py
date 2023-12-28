41d12dbe752a58dfc2f3c2f305d1c8ba51b1f945
ShadowRezi <urijozimko4@gmail.com>
2023-12-28 13:11:38 +0200

    Fix error of quiting app

--- manager.py
+++ manager.py
@@ -21,7 +21,7 @@ class BotControlApp:
         ctk.CTkButton(master, text='Start', command=self.start_bot).pack(pady=10)
         ctk.CTkButton(master, text='Stop', command=self.stop_bot).pack(pady=10)
         ctk.CTkButton(master, text='Restart', command=self.restart_bot).pack(pady=10)
-
+        ctk.CTkButton(master, text='Quit', text_color='#ea0002', bg_color='#ffffff', command=self.quit_app).pack(pady=10)
         self.is_bot_runned = False
         self.bot = None
 
@@ -54,7 +54,11 @@ class BotControlApp:
     def update_label(self):
         self.label.configure(
             text='Started' if self.is_bot_runned else 'Stopped'
-            )
+        )
+    
+    def quit_app(self):
+        self.stop_bot()
+        self.master.quit()
 
 
 if __name__ == "__main__":
