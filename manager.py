b5e29f441443ecf31ea669274d753435337627c0
ShadowRezi <urijozimko4@gmail.com>
2023-12-28 20:13:58 +0200

    .

--- manager.py
+++ manager.py
@@ -13,29 +13,37 @@ class BotControlApp:
 
         self.master = master
         self.master.title("Managing bot")
-        # self.master.config(bg='#072a47')
-
+       
         self.label = ctk.CTkLabel(master, text='Stopped')
         self.label.pack(pady=10)
 
-        ctk.CTkButton(master, text='Start', command=self.start_bot).pack(pady=10)
-        ctk.CTkButton(master, text='Stop', command=self.stop_bot).pack(pady=10)
-        ctk.CTkButton(master, text='Restart', command=self.restart_bot).pack(pady=10)
-        ctk.CTkButton(master, text='Quit', text_color='#ea0002', bg_color='#ffffff', command=self.quit_app).pack(pady=10)
+        ctk.CTkButton(
+            master, text='Start', command=self.start_bot
+        ).pack(pady=10)
+        ctk.CTkButton(
+            master, text='Stop', command=self.stop_bot
+        ).pack(pady=10)
+        ctk.CTkButton(
+            master, text='Restart', command=self.restart_bot
+        ).pack(pady=10)
+        ctk.CTkButton(
+            master, text='Quit', text_color='#ea0002', bg_color='#ffffff', command=self.quit_app
+        ).pack(pady=10)
 
         self.is_bot_runned = False
         self.bot = None
 
     def start_bot(self):
-        if not self.is_bot_runned:
-            self.is_bot_runned = True
-            self.update_label()
+        if self.is_bot_runned:
+            return
+            
+        self.is_bot_runned = True
+        self.update_label()
 
-            reload(userbot)
-            self.bot = Process(
-                target=userbot.main
-            )
-            self.bot.start()
+        reload(userbot)
+
+        self.bot = Process(target=userbot.main)
+        self.bot.start()
 
     def stop_bot(self):
         if self.is_bot_runned:
@@ -45,7 +53,7 @@ class BotControlApp:
             self.bot.terminate()
             self.bot.join()
 
-            print('Bot was stop!')
+            print('Bot was stopped!')
 
     def restart_bot(self):
         if self.is_bot_runned:
@@ -56,7 +64,7 @@ class BotControlApp:
         self.label.configure(
             text='Started' if self.is_bot_runned else 'Stopped'
         )
-    
+
     def quit_app(self):
         self.stop_bot()
         self.master.quit()
@@ -66,4 +74,3 @@ if __name__ == "__main__":
     root = ctk.CTk()
     app = BotControlApp(root)
     root.mainloop()
-
