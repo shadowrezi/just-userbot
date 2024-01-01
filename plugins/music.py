f7ac3e43f048a8bc8f2c97e32d0403a767498ae2
shadowrezi <129873777+shadowrezi@users.noreply.github.com>
2024-01-01 18:02:59 +0200

    Update music.py

--- plugins/music.py
+++ plugins/music.py
@@ -1,15 +1,61 @@
 import os
+from aiohttp import ClientSession
 
 from pyrogram import Client 
 from pyrogram.filters import me, private, command
 from pyrogram.types import Message
 
-from aiohttp import ClientSession
 from aiofiles import open
 from youtube_search import YoutubeSearch
 import yt_dlp
+from fake_useragent import UserAgent
 
 YDL_OPTIONS = {'format': 'bestaudio[ext=m4a]'}
+
+CAPTION = '<b>🎧 Uploader @ShadowRazea</b>'
+SONG_NOT_FOUND = "<b>❌ Song not found.\n\nPlease give a valid song name.</b>\n\nIf bot don't work, write me @ShadowRazea"
+DOWNLOADING_FILE = '<b>📥 Downloading file...</b>'
+UPLOADING_FILE = '<b>📤 Uploading file...</b>'
+ERROR = '<b>❌ Error, write to owner @ShadowRazea or add issue on <a href="https://github.com/shadowrezi/just-userbot">GitHub Repository</a></b>'
+
+
+async def search_video(query: str) -> tuple:
+        results = YoutubeSearch(query, max_results=1).to_dict()[0]
+    
+        title = results['title'][:40]
+        thumbnail = results['thumbnails'][0]
+        thumb_name = f'{title}.jpg'
+
+        headers = {'User-Agent': UserAgent().random}
+    
+        async with ClientSession(headers=headers) as session:
+            async with session.get(thumbnail, allow_redirects=True) as response:
+                thumb = await response.read()
+        
+        async with open(thumb_name, 'wb') as f:
+            await f.write(thumb)
+
+        return thumb, results, title
+
+
+async def download_video(results: dict) -> str:
+        link = f"https://youtube.com{results['url_suffix']}"
+        duration = results[0]['duration']
+    
+        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+            ydl.cache.remove()
+
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        
+        for i in dur_arr[::-1]:
+            dur += int(float(i)) * secmul
+            secmul *= 60
+
+        return audio_file, thumb_name, dur
 
 
 @Client.on_message(
@@ -20,69 +66,33 @@ YDL_OPTIONS = {'format': 'bestaudio[ext=m4a]'}
 )
 async def download_and_send_song(_: Client, message: Message):
     query = ' '.join(message.command[1:])
-    msg = await message.reply('**🔎 Finding song...**')
+    msg = await message.reply('<b>🔎 Finding song...</b>')
     
     try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        title = results[0]['title'][:40]
-        thumbnail = results[0]['thumbnails'][0]
-        thumb_name = f'{title}.jpg'
-        
-        async with ClientSession() as session:
-            async with session.get(thumbnail, allow_redirects=True) as response:
-                thumb = await response.read()
-        
-            async with open(thumb_name, 'wb') as f:
-                await f.write(thumb)
-        
-        duration = results[0]['duration']
+        thumb, results, title = await search_video(query)
+    except Exception as ex:
+        await msg.edit(SONG_NOT_FOUND)
+        raise ex
 
-    except Exception as e:
-        await msg.edit(
-            '''
-**❌ Song not found.\n\nPlease give a valid song name.**\n\nIf bot don't work, write me @ShadowRazea
-            '''.strip()
-        )
-        print(str(e))
-        return
-    await msg.edit('**📥 Downloading file...**')
-
+    await msg.edit(DOWNLOADING_FILE)
+    
     try:
-        with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-            ydl.cache.remove()
-
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-
-
-        rep = '<b>🎧 Uploader @ShadowRazea</b>'
-        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        audio_file, thumb_name, duration = await download_video(results)
         
-        for i in range(len(dur_arr) - 1, -1, -1):
-            dur += int(float(dur_arr[i])) * secmul
-            secmul *= 60
+        await msg.edit(UPLOADING_FILE)
 
-        await msg.edit('**📤 Uploading file...**')
         await message.reply_audio(
             audio_file,
-            caption=rep,
+            caption=CAPTION,
             thumb=thumb_name,
             title=title,
-            duration=dur,
+            duration=duration,
         )
         
         await msg.delete()
-    except Exception as e:
-        await msg.edit(
-            '''
-<b>❌ Error, write to owner @ShadowRazea or add issue on \
-<a href="https://github.com/shadowrezi/just-userbot">GitHub Repository</a>
-</b>
-'''.strip()
-        )
-        print(e)
+    except Exception as ex:
+        await msg.edit(ERROR)
+        raise ex
 
     finally:
         if os.path.isfile(thumb_name):
@@ -92,4 +102,3 @@ async def download_and_send_song(_: Client, message: Message):
 
     print(audio_file)
     print(thumb_name)
-
