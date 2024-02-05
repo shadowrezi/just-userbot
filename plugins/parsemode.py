2b3adaf8f1091b3fd59231c33a31f50594662f51
shadowrezi <129873777+shadowrezi@users.noreply.github.com>
2024-02-05 23:00:03 +0200

    Update parsemode.py

--- plugins/parsemode.py
+++ plugins/parsemode.py
@@ -3,17 +3,20 @@ from pyrogram.types import Message
 
 from misc.filters import in_message
 
-
+'''
 @Client.on_edited_message(
     in_message('<') &
     in_message('>') &
     filters.me
 )
 async def update_edited_parsemode(_: Client, message: Message):
-    '''Update edited message with html & markdown parsemodes'''
     await message.edit(message.text)
-
-
+'''
+@Client.on_edited_message(
+    in_message('<') &
+    in_message('>') &
+    filters.me
+)
 @Client.on_message(
     in_message('<') &
     in_message('>') &
