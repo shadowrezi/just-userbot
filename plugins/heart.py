6918f631274d54ba949ab4558eed69d4d7946bfa
shadowrezi <129873777+shadowrezi@users.noreply.github.com>
2024-02-05 23:12:55 +0200

    Update heart.py

--- plugins/heart.py
+++ plugins/heart.py
@@ -5,6 +5,14 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 
 
+async def edit_with_retry(message: Message, text: str, time: int | float = 0.5):
+    try:
+        await message.edit(text)
+        await sleep(time)
+    except FloodWait as fw:
+        await sleep(fw.value)
+
+
 @Client.on_message(
     filters.command(
         commands=["magic"],
@@ -28,24 +36,18 @@ async def mini_magic(_, message: Message):
 ✨✨✨💎✨✨✨
 """.strip()
 
-    frame_2 = base_frame.replace("💎", "🌺")
-    frame_3 = base_frame.replace("💎", "😘").replace("✨", "☁️")
-    frame_4 = base_frame.replace("💎", "🌸")
-    frame_5 = base_frame.replace("💎", "🐸").replace("✨", "🌾")
-    frame_6 = base_frame.replace("💎", "💥").replace("✨", "🔫")
-    frame_7 = base_frame.replace("💎", "💟").replace("✨", "☁️")
-    frame_8 = base_frame.replace("💎", "💖").replace("✨", "🍀")
-    frame_9 = base_frame.replace("💎", "🐼").replace("✨", "🌴")
-
-    frames = [base_frame, frame_2, frame_3, frame_4, frame_5, frame_6, frame_7, frame_8, frame_9]
-
-    async def edit_with_retry(message: Message, text: str, time: int | float = 0.5):
-        try:
-            await message.edit(text)
-            await sleep(time)
-        except FloodWait as fw:
-            await sleep(fw.value)
-
+    frames = [
+        base_frame,
+        base_frame.replace("💎", "🌺"),
+        base_frame.replace("💎", "😘").replace("✨", "☁️"),
+        base_frame.replace("💎", "🌸"),
+        base_frame.replace("💎", "🐸").replace("✨", "🌾"),
+        base_frame.replace("💎", "💥").replace("✨", "🔫"),
+        base_frame.replace("💎", "💟").replace("✨", "☁️"),
+        base_frame.replace("💎", "💖").replace("✨", "🍀"),
+        base_frame.replace("💎", "🐼").replace("✨", "🌴"),
+    ]
+    
     for frame in frames:
         await edit_with_retry(message, frame)
 
