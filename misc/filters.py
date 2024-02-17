8c1e0995112ce5c2b8ba5ebd864af23ebda1ace5
shadowrezi <129873777+shadowrezi@users.noreply.github.com>
2024-02-18 00:19:24 +0200

    Update filters.py

--- misc/filters.py
+++ misc/filters.py
@@ -1,31 +1,13 @@
-from functools import singledispatch
-
 from pyrogram import filters
 from pyrogram.types import Message
 
-@singledispatch
-def in_message(text):
-    pass
 
-@in_message.register
-def _(text: str):
+def in_(text: str):
     async def filter(flt, _, message: Message):
         if message.text is None:
             return False
         return flt.text in message.text
     return filters.create(filter, text=text)  # text=text, we get it by `flt`
-
-@in_message.register
-def in_message(text: list):
-    async def filter(flt, _, message: Message):
-        if message.text is None:
-            return False
-        
-        for i in flt.text:
-            if i in message.text:
-                return False
-        return True
-    return filters.create(filter, text=text)
 
 
 def startswith(target: str):
